81c3780f15274ef8c9654bf527d17fb4858105f7
etckanikama <pingshandakui@gmail.com>
2023-06-16 17:20:28 +0900

    gazebo環境におけるいらないmaterialファイルを削除

--- create_white_map_opencv.py
+++ create_white_map_opencv.py
@@ -6,7 +6,8 @@ width = 662
 height = 1263
    
 # 白い背景の画像を作成
-image = np.full((height, width, 3), (65, 105, 225), dtype=np.uint8)
+# image = np.full((height, width, 3), (65, 105, 225), dtype=np.uint8)
+image = np.full((height, width, 3), (0, 0, 0), dtype=np.uint8)
 
 # 矩形の座標を辞書型で定義
 rect_dict = {0: [[246.5, 350.5], [254.0, 350.5], [254.0, 1263.0], [246.5, 1263.0]],
@@ -26,6 +27,26 @@ for key in rect_dict.keys():
     rect = np.array([rect_dict[key]], dtype=np.int32)
     cv2.fillPoly(image, rect, (255, 255, 255))
     # cv2.putText(image, str(key), tuple(map(int, rect_dict[key][0])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+# 画像内で白情報のcv座標を取得
+print(image.shape)
+h, w,c = image.shape
+print(h, w,c)
+print(type(image))
+print(image[0,0])
+# 白色ピクセルの座標を格納するリスト
+white_pixels = []
+
+for y in range(height):
+    for x in range(width):
+        b, g, r = image[y,x]
+        if b == 255 and g == 255 and r == 255:
+            white_pixels.append((x,y))
+
+ 
+print(image[1262,355])
+# for coodinate in white_pixels:
+#     print(coodinate)
 
 # 画像を表示
 cv2.imshow("SB_WhiteLaneMap", image)
--- hihuku_problem_sample.py
+++ hihuku_problem_sample.py
@@ -4,12 +4,16 @@ import matplotlib.patches as patches
 import random, math, time
 import pulp
 
-# 入力データ作成、利用者・施設設置可能点生成
-def make_data(n, upper):
-    random.seed(5)
-    X = [random.randint(0, upper) for i in range(n)]
-    Y = [random.randint(0, upper) for i in range(n)]
-    return [X, Y]
+# マーカーを置く候補の座標を10cm間隔で配置
+def make_data():
+    # random.seed(5)
+    # X = [random.randint(8, upper) for i in range(n)]
+    X = np.arange(8, 11, 0.1)
+    Y = np.zeros(30)
+    # print(X.__len__())
+    # Y = [random.uniform(-1, 1) for i in range(n)]
+    # print(Y)
+    return [Y, X]
 
 # 利用者(p1)と施設(p2)の距離を測る
 def distance(p1, p2):
@@ -36,11 +40,10 @@ def make_matrix(ps, r):
 def draw(ps, fs, r):
     fig = plt.figure(figsize=(7,7))
     ax = plt.axes()
-    x = np.arange(0, 80, 0.1)
     plt.scatter(ps[0], ps[1], s=20) # 利用者
 
     for k, (i,j) in zip(fs, zip(ps[0], ps[1])):
-        if k.value() == 1: # 施設を設置する場合
+        if k.value() == 1: # マーカーを設置する場合
             plt.scatter(i, j, s=20, color='red')
             c = patches.Circle(xy=(i, j), radius=r, fill=False) # カバー範囲
             ax.add_patch(c)
@@ -48,12 +51,28 @@ def draw(ps, fs, r):
             pass
 
     plt.axis('scaled')
+    plt.xticks(range(-5,5,1))
+    plt.yticks(range(6,15,1))
     ax.set_aspect('equal') # 縦横比を調整
-    plt.grid()
+    plt.grid(linestyle='dotted')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
-    ax.set_xticks(np.linspace(0, 80, 9))
-    ax.set_yticks(np.linspace(0, 80, 9))
+
+    ax.hlines(y=10.095, xmin=-0.47, xmax=-5.0, color='red', linestyles='solid')
+    ax.hlines(y=9.125, xmin=0.47, xmax=1.935, color='red', linestyles='solid')
+    ax.hlines(y=11.28, xmin=1.76, xmax=3.01, color='red', linestyles='solid')
+    ax.hlines(y=10.205, xmin=1.935, xmax=3.01, color='red', linestyles='solid')
+    ax.hlines(y=11.41, xmin=0.75, xmax=-5.0, color='red', linestyles='solid')
+
+
+
+    ax.vlines(x=0.47,ymin=0, ymax=9.125,colors='red',linestyles='solid')
+    ax.vlines(x=-0.47,ymin=0, ymax=10.095,colors='red',linestyles='solid')
+    ax.vlines(x=1.935,ymin=9.125, ymax=10.205,colors='red',linestyles='solid')
+    ax.vlines(x=1.76,ymin=11.28, ymax=13.5,colors='red',linestyles='solid')
+    ax.vlines(x=0.75,ymin=11.41, ymax=13.5,colors='red',linestyles='solid')
+
+    ax.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0)
     plt.show()
 
 # 解く
@@ -69,16 +88,19 @@ def solver_set(ps, r):
     s = time.time() # 時間計測
     status = prob.solve() # 問題が解けたのか、状態確認
     print('Status', pulp.LpStatus[status])
-    print('z 施設数：', prob.objective.value())
+    print('z マーカー数：', prob.objective.value())
     print('処理時間：', time.time() - s)
     draw(ps, fs, r)
 
-n = 25 # 利用者・施設を合わせた地点数
-r = 20 # 半径
-upper = 80 # 乱数の上限値
-solver_set(make_data(n, upper), r)
-
+# n = 20 # 利用者・施設を合わせた地点数
+r = 0.2 # 半径
+# upper = 12 # 乱数の上限値
+solver_set(make_data(), r)
+# make_data(n,upper)
 # 結果
 # Status Optimal
 # z 施設数： 9.0
 # 処理時間： 0.05631732940673828
+
+# この半径rをどう決めるかが大事
+# →カメラが見える範囲4m？
